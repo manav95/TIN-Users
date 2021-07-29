69f0befea17916d34570c1b594275b3cd40f441c
Manav Dutta <Manav.Dutta@capgroup.com>
2021-07-28 17:39:33 -0700

    commit tin blog changes

--- tin-blog/src/components/Navigation/Navigation.tsx
+++ tin-blog/src/components/Navigation/Navigation.tsx
@@ -5,6 +5,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import tinIcon from './tin-icon.png';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -22,15 +23,9 @@ function Navigation(props) {
   const classes = useStyles();
 
   return (
-    <AppBar position="static" className={classes.root}>
+    <AppBar position="relative" className={classes.root}>
       <Toolbar>
-        <img src={"../../images/tin-icon.png"} alt="website logo"/>
-        <Typography
-          variant="h6"
-          className={classes.title}
-        >
-          {props.siteTitle}
-        </Typography>
+        <img src={tinIcon} alt={props.siteTitle}/>
         <div>
             <Button
               component={Link}
